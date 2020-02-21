f29b8e38d7774c21438072b5bcbb0d3ba8655d14
Lisa <lisaqdebona@gmail.com>
2020-02-20 23:44:28 -0700

    Fixed issues on locations page

--- inc/extras.php
+++ inc/extras.php
@@ -143,12 +143,7 @@ function email_obfuscator($string,$noFilter=null) {
                     $rep2 = $em.'</a>';
                     $new2 = antispambot($em).'</a>';
                     $string = str_replace($rep2, $new2, $string);
-                } else {
-                    // $new_mailto = '<a href="mailto:'.antispambot($em,1).'">'.antispambot($em).'</a>';
-                    // $string = str_replace($em, $new_mailto, $string);
-                }
-
-                
+                } 
             }
         }
         if($noFilter) {
