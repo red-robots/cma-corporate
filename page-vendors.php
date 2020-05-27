9a1a6342e5035a8d1e91a3360eb8861ba37d2a17
Lisa <lisaqdebona@gmail.com>
2020-05-26 22:21:50 -0600

    Modified page-vendors.php

--- page-vendors.php
+++ page-vendors.php
@@ -10,7 +10,7 @@
 get_header(); ?>
 <div class="vendor-page-content cf">
 
-    <!-- 1st row -->
+    <!-- Main Text -->
     <?php
     $row1_title     = get_field('vendors_main_title');
     $row1_text      = get_field('vendors_main_text');  
@@ -54,7 +54,7 @@ get_header(); ?>
     <?php } ?>
 
 
-  <!-- 2nd row -->
+  <!-- Preferred Vendor -->
   <?php
     $row_2_title     = get_field('row_2_title');
     $row_2_text      = get_field('row_2_text');        
@@ -67,23 +67,21 @@ get_header(); ?>
       <div class="justify-content-center">
         <div class="mb-2 mt-4 col-md-7" style="margin: 0 auto;" >
           <div class="fadeIn wow" data-wow-delay=".9s">
-            <h1  class="text-center">
-              <?php echo $row_2_title; ?>
-            </h1>
+            <h1  class="text-center"><?php echo $row_2_title; ?></h1>
           </div>
         </div>
       </div>
       <?php } ?>
       
+      <?php if ($row_2_text) { ?>
       <div class="container">
         <div class="row">
           <div class="col-md-7 cma-center text-center">
-              <div class="fadeIn wow" data-wow-delay=".9s">
-                  <?php echo ($row_2_text) ? $row_2_text : '';  ?>
-              </div>
+              <div class="fadeIn wow" data-wow-delay=".9s"><?php echo $row_2_text ?></div>
           </div>
         </div> <!-- row -->
       </div> <!-- container -->
+      <?php } ?>
 
       <?php /* Preferred Vendors posts */ ?>
       <?php get_template_part("template-parts/vendors-preferred"); ?>
@@ -92,78 +90,8 @@ get_header(); ?>
   </div>
   <?php } ?>
 
-  <!-- 3rd row -->
-  <?php  
-  	$row_3_image_text = get_field('row_3_image_header_text');
-    $row_3_image     = get_field('row_3_image');
-  	$row_3_text      = get_field('row_3_text');
-  	$row_3_offers    = get_field('row_3_offers');
-  ?>
-  
-  <?php if ($row_3_image_text || $row_3_image) { ?>
-  <div class="text-center header_image_section fadeIn wow" data-wow-delay="0.5s">    
-      <div class="featured_image " style="background-image: url('<?php echo $row_3_image['url'];  ?>');" >       
-      </div>     
-      <div class="header_image_text fadeIn wow" data-wow-delay="0.7s">
-        <h1 class=" align-middle">
-          <?php echo ($row_3_image_text) ? $row_3_image_text : '';  ?>
-        </h1> 
-      </div>      
-  </div>
-  <?php } ?>
 
-  
-  <?php if ($row_3_text || $row_3_offers) { ?>
-  <div class=" mb-5">
-    <div class="container text-center">
-      <div class="justify-content-center">
-        
-        <div class="col-md-8 cma-center pt-5">          
-          <div class="cma-paragraph-normal fadeInUp wow" data-wow-delay="0.5s">
-            <?php echo ($row_3_text) ? $row_3_text : '';  ?> 
-          </div>
-          <div class="row pt-4 ">
-			<?php if($row_3_offers): $x = 0; ?>
-				<?php foreach($row_3_offers as $offers): ?>
-					<?php
-						$x++;
-						$offer_icon 	= $offers['offer_image'];
-						$offer_title 	= $offers['offer_title'];
-						$offer_text 	= $offers['offer_text'];
-					?>
-			          <div class="col-sm-4 fadeInUp wow" data-wow-delay="<?php echo ($x / 5) . 's'; ?>">
-			            <div class="cma-icon-holder">
-			            	<?php if($offer_icon): ?>
-			              		<img src="<?php echo $offer_icon['url']; ?>" alt="">
-			          		<?php endif; ?>
-			            </div>
-			            <div class="cma-sub-title ">
-			              <?php echo ($offer_title) ? $offer_title : ''; ?>
-			            </div>
-			            <p class="cma-paragraph-normal">
-			              <?php echo ($offer_text) ? $offer_text : ''; ?>
-			            </p>
-			          </div>
-	          	<?php endforeach; ?>
-	      	<?php endif; ?>
-
-			
-
-        </div>
-
-          
-
-        </div>
-
-
-
-      </div>
-      
-    </div>
-  </div>
-  <?php } ?>
-
-  <!-- 4th row -->
+  <!-- Partner Vendor -->
   <?php
   	$row_4_title 		= get_field('row_4_title');
   	$row_4_text 		= get_field('row_4_text');
@@ -225,111 +153,8 @@ get_header(); ?>
   </div>
   <?php } ?>
 
-  <!-- 5th row -->
-  <?php
-  	$row_5_title 		= get_field('row_5_title');
-  	$row_5_text 		= get_field('row_5_text');
-  	$row_5_monitoring 	= get_field('row_5_monitoring');
-  ?>
-
-  <?php if ($row_5_title || $row_5_text) { ?>
-  <div class="row-5" id="why_cma">
-    <div class="container text-center">
-      <div class="col-md-8 cma-center fadeIn wow" data-wow-delay="0.7s">
-      	
-        <?php if ($row_5_title) { ?>
-        <div class="cmatitlediv">
-          <h1 class="cma-title-red"><?php echo $row_5_title ?></h1>
-        </div>
-        <?php } ?>
-
-        <?php if ($row_5_text) { ?>
-        <div class="mt-4 mb-4">
-          <p class="cma-paragraph-why"><?php echo $row_5_text ?></p>
-        </div>
-        <?php } ?>
-        
-      </div>
-
-		<?php if($row_5_monitoring):  $x = 0; ?>
-			<?php foreach($row_5_monitoring as $monitor): ?>
-				<?php
-					$x++;
-					$monitor_icon = $monitor['monitoring_image'];
-					$monitor_title = $monitor['monitoring_title'];
-				?>
-		      <div class="cma-center cma-why-container fadeInUp wow" data-wow-delay="<?php echo ($x / 5) . 's'; ?>">
-		        <div class="cma-icon-black">
-		        	<?php if($monitor_icon): ?>
-		          		<img src="<?php echo $monitor_icon['url'];  ?>" alt="">
-		          	<?php endif; ?>
-		        </div>
-		        <div class="cma-title-dark">
-		          <?php echo ($monitor_title) ? $monitor_title : '';  ?>
-		        </div>        
-		      </div>
-	  		<?php endforeach; ?>
-      	<?php endif; ?>
-
-    </div>
-  </div>
-  <?php } ?>
-
-  <!-- 6th row -->
-  <?php
-	$row_6_title 		= get_field('row_6_title');
-	$row_6_text 		= get_field('row_6_text');
-	$row_6_certificates = get_field('row_6_certificates');
-	$row_6_button_title = get_field('row_6_button_title');
-	$row_6_button_link 	= get_field('row_6_button_link');
-  $row6Contents = array($row_6_title,$row_6_text,$row_6_certificates);
-  $isMulticolored = ($row6Contents && array_filter($row6Contents)) ? 'whiteBg':'multicolored';
-  ?>
-
-  <?php if ($row_6_title || $row_6_text || $row_6_certificates) { ?>
-  <div class=" cma-bg-mixed">
-    <div class="container text-center pb-5">
-      <div class="col-md-8 cma-center  pt-5">
-
-        <?php if($row_6_title) { ?>
-      	<div class="cma-title-wrap fadeIn wow" data-wow-delay=".8s">
-      		<h1 class="cma-title-white pb-4"><?php echo $row_6_title ?></h1>
-      	</div>
-        <?php } ?>
-
-        <div class="textWrap fadeInUp wow" data-wow-delay=".9s">
-        
-          <?php if($row_6_text) { ?>
-          <div class="cma-paragraph-white text-white"><?php echo $row_6_text ?></div>
-          <?php } ?>
-          
-          <?php if($row_6_certificates) { ?>
-          <div class="text-center text-white cert_lists mt-3">
-  	          <ul class="list-group">
-  	          	<?php foreach($row_6_certificates as $x=>$certificate) { ?>
-  	            <li class="list-group-item"><?php echo ($certificate['certificate_title']) ? $certificate['certificate_title'] : ''; ?></li>
-  	            <?php } ?>	            
-  	          </ul>
-          </div>
-          <?php } ?>
-          
-          <?php if ($row_6_button_link && $row_6_button_title) { ?>
-          <div class="mt-4">
-            <a href="<?php echo $row_6_button_link; ?>" class="btn-gray"><?php echo $row_6_button_title; ?></a>
-          </div>
-          <?php } ?>
-
-        </div>
-        
-
-      </div>
-
-    </div>
-  </div>
-  <?php } ?>
-
   
-  <!-- 7th row -->
+  <!-- Request Vendor -->
   <?php
   	$row_7_title 		= get_field('row_7_title');
   	$row_7_text 		= get_field('row_7_text');
@@ -338,24 +163,20 @@ get_header(); ?>
   ?>
 
   <?php if ($row_7_title || $row_7_text) { ?>
-  <div class="pt-3 pb-5 <?php echo $isMulticolored ?>" id="request_information">
+  <div class="pt-3 pb-5 multicolored" id="request_information">
     <div class="container text-center">
-      <div class="col-md-8 cma-center">  
+      <div class="col-md-8 cma-center fadeIn wow" data-wow-delay="0.8s">  
 
         <?php if ($row_7_title) { ?>
-        <div class="fadeIn wow" data-wow-delay="0.8s">     
-          <h1 class="cma-title-normal"><?php echo $row_7_title; ?></h1>
-        </div>
+        <h1 class="cma-title-normal"><?php echo $row_7_title; ?></h1>
         <?php } ?>
       	
         <?php if ($row_7_text) { ?>
-        <div class="cma-paragraph-normal fadeInUp wow" data-wow-delay=".9s">
-          <?php echo $row_7_text; ?>
-        </div>
+        <div class="cma-paragraph-normal"><?php echo $row_7_text; ?></div>
         <?php } ?>
         
         <?php if ($row_7_button_text && $row_7_button_link) { ?>
-        <div class="btndiv fadeIn wow animated" data-wow-delay="0.8s" >
+        <div class="btndiv">
           <a href="<?php echo $row_7_button_link; ?>" class="link-white"><?php echo $row_7_button_text; ?></a>
         </div>
         <?php } ?>
